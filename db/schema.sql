50a4ab13719898483f3a38d86446b949d663c601
Kevin McAteer <kmcateer926@gmail.com>
2020-12-10 20:13:53 -0500

    set up starter table

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,14 @@
 DROP DATABASE IF EXISTS tungsten_db;
 CREATE DATABASE tungsten_db;
+
+USE tungsten_db;
+
+CREATE TABLE tools
+(
+	id int NOT NULL AUTO_INCREMENT,
+	name varchar(255) NOT NULL,
+    available BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+);
+
+SELECT * FROM tools;
---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO tools (name, available) VALUES ('Drill', true);
+INSERT INTO tools (name, available) VALUES ('Hammer', true);
+INSERT INTO tools (name, available) VALUES ('Saw', true);
+INSERT INTO tools (name, available) VALUES ('Hacksaw', true);
+INSERT INTO tools (name, available) VALUES ('Monkeywrench', true);
+INSERT INTO tools (name, available) VALUES ('Hatchet', true);
+INSERT INTO tools (name, available) VALUES ('Pliers', true);
+INSERT INTO tools (name, available) VALUES ('Chainsaw', true);
+INSERT INTO tools (name, available) VALUES ('Anvil', true);
+INSERT INTO tools (name, available) VALUES ('Soldering iron', true);
+INSERT INTO tools (name, available) VALUES ('Circular saw', true);
+INSERT INTO tools (name, available) VALUES ('Workbench', true);
+INSERT INTO tools (name, available) VALUES ('Tape measure', true);
+INSERT INTO tools (name, available) VALUES ('Ladder', true);
