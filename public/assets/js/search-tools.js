978ba7aeb3770a4f46dee0296341c4a1cea69e97
Tara Martin <martin.tara117@gmail.com>
2020-12-13 18:20:24 -0500

    added document.querySelector and addEventListener inside serach-tools.js for the search-tools.handlebars. created toolSearch function inside search-tools.js that will collect user's search term, make an ajax call and direct to a callback function

--- public/assets/js/search-tools.js
+++ public/assets/js/search-tools.js
@@ -1,1 +1,14 @@
 console.log("search-tools.js loaded");
+
+document
+  .querySelector("#searchNav button")
+  .addEventListener("click", toolSearch);
+
+function toolSearch() {
+  //1.collect user's search term
+  let searchInput = document.querySelector("#searchNav input");
+  let searchTerm = searchInput.value;
+  searchInput.value = "";
+  //2.make ajax call
+  //3.direct to callback function
+}
