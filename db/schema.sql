a2e52270ba680ea046b2d08604b224758f66bb4c
Kevin McAteer <kmcateer926@gmail.com>
2020-12-18 13:52:01 -0500

    new tools page is now working, need tool information to populate

--- db/schema.sql
+++ db/schema.sql
@@ -1,18 +1,9 @@
-DROP DATABASE IF EXISTS tungsten_db;
 CREATE DATABASE tungsten_db;
 
 USE tungsten_db;
 
-CREATE TABLE tools
-(
-	id int NOT NULL
-	AUTO_INCREMENT,
-	name varchar
-	(255) NOT NULL,
-    available BOOLEAN DEFAULT false,
-	PRIMARY KEY
-	(id)
-);
+SELECT * FROM profiles;
 
-	SELECT *
-	FROM tools;
+SELECT * FROM tools;
+
+SELECT * FROM usertools;
